aa98c43c89a3f499f78f23dc5763904a78a9d088
arcbarkzap <ejconnell06@gmail.com>
2024-11-30 14:56:26 -0700

    kill off river since it won't start on nvidia, add grub theme

---
+++ files/scripts/set-grub.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+# Clone the repo
+git clone https://github.com/Lxtharia/minegrub-world-sel-theme.git
+cd ./minegrub-world-sel-theme
+sudo cp -ruv ./minegrub-world-selection /boot/grub/themes/
+echo "GRUB_THEME=/boot/grub/themes/minegrub-world-selection/theme.txt" >> /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
